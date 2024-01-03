20634c7694d6df63ae66d3e7f7a6f247611882d8
Loek van der Linde <lind0096@hz.nl>
2024-01-03 10:37:30 +0100

    4

--- app/Http/Controllers/OwlController.php
+++ app/Http/Controllers/OwlController.php
@@ -15,7 +15,7 @@ class OwlController extends Controller
     {
         $owls = Owl::orderBy('scheduled_at', 'asc')->get();
         $grades = Grade::all();
-        dd($grades);
+        dd($grades[0]->mark);
         return view('owls.index', [
             'owls' => $owl,
             'average' => $average

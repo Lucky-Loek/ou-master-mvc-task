8a78d381b395dd7c3e0ee8018ff497efcde1ae03
Loek van der Linde <lind0096@hz.nl>
2024-01-03 10:59:30 +0100

    9

--- resources/views/owls/show.blade.php
+++ resources/views/owls/show.blade.php
@@ -24,7 +24,6 @@
                 </div>
 
                 <pre>
-                    @dd($owl->grades[0]->mark)
                     @dd(array_reduce($owl->grades, function($carry, $item) { return $carry + $item->mark; }))
                 </pre>
             </div>

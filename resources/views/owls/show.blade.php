0ba817accf102c9fef4de94045632aab1dabec03
Loek van der Linde <lind0096@hz.nl>
2024-01-03 11:06:49 +0100

    18

--- resources/views/owls/show.blade.php
+++ resources/views/owls/show.blade.php
@@ -24,7 +24,7 @@
                 </div>
 
                 <pre>
-                    @dd($average_grade)
+                        {{ $average_grade }}
                 </pre>
             </div>
         </div>

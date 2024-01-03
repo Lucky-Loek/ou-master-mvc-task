b4bb4acb4dc30f47a80f07d0f6424ea6fa6887c3
Loek van der Linde <lind0096@hz.nl>
2024-01-03 11:24:38 +0100

    2

--- resources/views/owls/index.blade.php
+++ resources/views/owls/index.blade.php
@@ -35,6 +35,7 @@
                                         {{ $owl->name }}
                                         </a>
                                     </td>
+                                    <td></td>
                                     <td><span class="tag">{{ $owl->version_nr }}</span></td>
                                     <td>{{ $owl->scheduled_at->toDayDateTimeString() }}</td>
                                 </tr>
